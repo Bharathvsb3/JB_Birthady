1589e360cb654d87978c3198438bd0a4ad714fd4
Bharath <bharath.s@ciglobalsolutions.com>
2026-03-05 23:24:37 +0530

    Word change

--- config.js
+++ config.js
@@ -11,15 +11,15 @@
 
     login: {
         title: "Hi Thangam 💙",
-        subtitle: "Un 15-step Birthday Journey unlock pannalama? 🎁",
+        subtitle: "Birthday Journey unlock pannalama? 🎁",
         hint: "Namma secret PIN enter pannu 💙<br><span style='font-size:.78rem;opacity:.55;'>Hint: Namma special date 🗓️</span>",
-        success: "Welcome ma 💙",
-        error: "Idhu namma PIN illa di 😜"
+        success: "Welcome di 💙",
+        error: "Idhu namma PIN illa di vennamavalee😜"
     },
 
     mic: {
-        magics: ['i love you', 'love you', 'loveyou', 'ily', 'thangam', 'hi', 'hai', 'hello', 'hey', 'i love u', 'love u', 'naan unnai kadhalikkiren', 'jenifar', 'jenny', 'chellam', 'babe', 'baby'],
-        successMsg: "Nee sonna andha vaarthai enaku romba mukkiyam 🥹 Naanum unna romba love panren Thangam 💙 Innum oru surprise iruku 🎁",
+        magics: ['i love you', 'love you', 'loveyou', 'thangam', 'i love u', 'love u', 'naan unnai kadhalikkiren'],
+        successMsg: "Eee😁 Love you too 🥹 Innum oru surprise iruku 🎁",
         notSupported: "Mic support illa 😔 🎤 10 thadava click panni continue pannu!",
         denied: "Mic permission kudukala 🛑 🎤 10 thadava click pannu.",
         error: "Konjam error vandhuduchi 😅 Innum oru thadava try pannu illa 🎤 10 click pannu.",
@@ -30,9 +30,9 @@
     final: {
         countdownMsg: ["Ready ah irukiya…", "Konjam wait pannuu…", "Idhu unakkaga…"],
         hbdMsg: "💙 Happy Birthday En Thangam 👑",
-        subTitle: "HAPPY BIRTHDAY MA",
+        subTitle: "HAPPY BIRTHDAY JENIFARUUUU",
         ageLabel: "YEARS OF CUTENESS",
-        message: "<b>En Uyir {NAME},</b><br><br>Un life un ishtapadi amaiyanum nu daily pray panren. Nee enaku love mattum illa… nee enaku strength, peace, motivation.<br><br>Enna nadandhalum naan un kooda dhaan iruppen. Support panni, protect panni, love pannitu dhaan iruppen.<br><br>Forever un Thangam 💙",
+        message: "<b>En chellam {NAME},</b><br><br>Un life un ishtapadi amaiyanum. Nee enaku romba importent. <br><br>Enna nadandhalum naan un kooda dhaan iruppen. Support panni, protect panni, love pannitu dhaan iruppen.<br><br>Forever un Thangam 💙",
         btnText: "Tap to meet something interesting ✨"
     },
 
@@ -42,7 +42,7 @@
             nav: 'tap',
             icon: '💙',
             title: 'Hi En Thangam 💙',
-            txt: "Happy Birthday {NAME}! 🎂\nIdhu just steps illa ma… namma story.\nBayam la start aagi forever varai pona love 💙",
+            txt: "Happy Birthday {NAME}! 🎂\nIdhu just steps illa ma… namma story.\nBayam la start aagi forever vara pona love 💙",
             trick: "Screen ah tap pannu 👆"
         },
 
@@ -53,16 +53,16 @@
             txt: "Indha naal la dhaan indha ulagathuku oru azhagana ponnu vandha.\nAparam konjam varushathuku apram… en life la vandha 💙",
             liveAge: 'A',
             img: 'IMG20230312122458.jpg',
-            trick: "Phone shake pannu 📳"
+            trick: "Phone shake pannu di📳"
         },
 
         {
             nav: 'doubletap',
             icon: '👀',
             head: 'College Days 📖',
-            txt: "Same college… same time.\nAana unakku en per kooda theriyadhu.\nNaan tholaivula irundhu paathutu irundhen 🙈",
+            txt: "Same college… same time.\nAana unakku en per kooda theriyadhu.\n Nanum unna pakkala but 2021 la en focus un mela vandhuchi. Naan tholaivula irundhu paathutu irundhen 🙈",
             img: '1.jpg',
-            trick: "Double tap pannu ✌️"
+            trick: "Double tap pannu ma✌️"
         },
 
         {
@@ -78,7 +78,7 @@
             nav: 'longpress',
             icon: '📱',
             head: 'August 2021 – First Hi 💙',
-            txt: "August 2021… sema bayathula naan first 'Hi' anupinen.\nKai aadi pochu ma.\nReply pannuva nu kooda theriyala.\n\nAana nee reply panna moment… naan romba happy.\nAndha small message en life la periya change 💙",
+            txt: "August 2021… sema bayathula naan first 'Hi' anupinen.\nKai aadi pochu. Nee enna pathi enna nenaipanu kuda theroyala but msg pannitan.\n\nAana nee reply panna moment… naan romba happy.\nAndha small message en life la periya change 💙",
             img: 'IMG20230129154348.jpg',
             trick: "2 sec long press pannu 👆"
         },
@@ -87,7 +87,7 @@
             nav: 'swipeleft',
             icon: '💘',
             head: 'First I Love You 💘',
-            txt: "Konjam naal pesina apram… naan dhaan first love sonnen.\n'I love you.'\nEn heart sema beat.\nNee appo edhum sollala.\nAndha silence enaku bayama irundhudhu di.\nAana naan wait panna ready ah irundhen.",
+            txt: "Konjam naal pesina apram… naan dhaan first love ahh sonnen.\n'I love you.'\nEn heart sema beat.\nNee appo prank pannuranu nenicha nanum samalichan.\nAndha silence enaku bayama irundhudhu di.\nAana naan wait panna unakaga.",
             img: '4.jpg',
             trick: "Swipe left pannu ⬅️"
         },
@@ -96,7 +96,7 @@
             nav: 'tap3',
             icon: '⏳',
             head: 'Andha Oru Week 💔',
-            txt: "First okay sonna.\nAana 1 week apram 'No' sonna.\nAndha week enaku romba kashtam.\nVelila kaamikala… aana ullae romba hurt aayiten.\nStill… nee dhaan nu nambinen.",
+            txt: "First okay sonna.\nAana 1 week apram 'No' sonna.\nAndha week enaku romba kashtama irundhadhu.\nStill… nee dhaan nu nambinen.",
             trick: "3 thadava tap pannu 👆"
         },
 
@@ -113,24 +113,24 @@
             nav: 'iloveyou',
             icon: '🎤',
             head: 'Ippo Nee Sollu 💙',
-            txt: "Ippo phone ah close ah pudichu…\nAndha 3 vaarthai sollu ma 🥹",
-            trick: "Say 'I Love You' illa 🎤 10 click pannu!"
+            txt: "Ippo phone ah close ah pudichu…\nAndha 3 vaarthai sollu di 😁",
+            trick: "Say 'I Love You' "
         },
 
         {
             nav: 'pinch',
             icon: '🎵',
             head: 'Namma Song 🎶',
-            txt: "Indha song ketta udane enaku nee dhaan nyabagam varuva.\nIdhu namma feel 💙",
+            txt: "Indha song ketta udane enaku nee dhaan nyabagam varuva.\n edha play panni kelu unaku pudikum nu namburan. \nEdha play panna 🎵 click pannu play aagum di. 💙",
             music: true,
-            trick: "Play panni pinch pannu 🎵"
+            trick: "Play panni pinch🤏 pannu 🎵"
         },
 
         {
             nav: 'spread',
             icon: '✈️',
             head: 'Namma Future ✨',
-            txt: "Enga ponaalum parava illa.\nNee kooda irundha podhum.\nEvery place paradise dhaan 🌍",
+            txt: "Enga ponaalum parava illa.\nNee kooda irundha podhum.\nEvery place paradise dhaan 🌍. Nee nenaikura mari illadi nanu. \nNee sollura ellam kekkuran but enaku theriyala aadha eppadi project pannuradhuuu.\n Ne venum aadhuku na enna effort pottalum aadhu edhachum problem vandhu mudinjidudhu. \n But onnu ennala munjavara vidama effort poduvan.",
             img: '3.jpg',
             trick: "Screen la fingers spread pannu 🤲"
         },
@@ -148,11 +148,10 @@
             nav: 'rotate',
             icon: '💼',
             head: 'Un Support 💙',
-            txt: "College mudinju apram nee dhaan sonna IT job try pannu nu.\nNee nambina.\nAdhanala naan try pannen.\nInniku nalla job, nalla salary la iruken.\nIdhuku nee dhaan reason ma 💙",
+            txt: "College mudinju apram nee dhaan sonna IT job try pannu nu.\nNee nambina.\nAdhanala naan try pannen.\nInniku nalla job, nalla salary la iruken.\nIdhuku nee dhaan reason ma 💙. \nBut nee enna nambu na eppadiyavadhu nee expect pannura mari oru nalla possion ku varuvan. \nNaan en mela confidence illama irukura nerathula nee en mela confidence vechi enaku support ahh erudi.",
             img: 'Picsart_26-01-16_22-59-05-218.jpg',
             trick: "Phone rotate pannu ↺"
         },
-
         {
             nav: 'twofingertap',
             icon: '🧩',
